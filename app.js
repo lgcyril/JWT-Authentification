fcdee8be1dbfa9d0437c708dbc697a5ad594053a
Cyril Luong <lgcyriltt@gmail.com>
2023-02-17 16:30:39 +0100

    generate Access Token Ok

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-console.log("Hello world")
+
+
+// console.log("Secret is " + process.env.ACCESS_TOKEN_SECRET)
+const user = { 
+    id: 45,
+    name: "John Doe",
+    email: "jd@fbi.gov",
+    admin: true,
+}
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+const accessToken = generateAccessToken(user);
+console.log("Access Token is ", accessToken);
